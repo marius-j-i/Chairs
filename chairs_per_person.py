2e031ef5f38c61f84793d620730b9fc463104cb8
Marius, Frigate Absolute <min016@uit.no>
2020-02-11 15:30:13 +0100

    BAM..

--- chairs_per_person.py
+++ chairs_per_person.py
@@ -1,6 +1,13 @@
-f = open("reservations.csv")
+f = open("reservations")
+no_eat = {}
 
 for reservation in f:
-    name, number = reservation.split(",")
-    chairs_per_person = 50 / int(number)
-    print("{} will get {} chairs per person".format(name, chairs_per_person))
+	name, number = reservation.split(",")
+	try:
+		chairs_per_person = round(50 / int(number))
+	except:
+		#print("Wabbit-{} can't eat here. ".format(name))
+		no_eat[name] = number 
+	print("{} will get {} chairs per person".format(name, chairs_per_person))
+
+print(no_eat)
